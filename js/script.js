0c64a80dad4603e60bc8ad89bec8e8f067969820
shanin18 <112709243+shanin18@users.noreply.github.com>
2023-03-03 01:45:59 +0600

    added all the card details

--- js/script.js
+++ js/script.js
@@ -7,25 +7,49 @@ const loadAllData = async () =>{
 }
 
 // display all Data
-const displayAllData = (data) =>{
+const displayAllData = (data) => {
     const cardsContainer = document.getElementById("cards-container");
     data.forEach(element => {
-        console.log(element)
         const card = document.createElement("div");
+        console.log(element)
+        // create the ol element
+        const ol = document.createElement("ol"); 
+        element.features.forEach((feature, index) => {
+            const li = document.createElement("li");
+            li.classList.add("text-[#585858]", "mb-2")
+            li.innerText = `${index+1}. ${feature}`;
+            ol.appendChild(li);
+        });
+
         card.innerHTML = `
-        <div class="card bg-base-100 shadow-xl border p-6 rounded-xl">
-            <figure>
-                <img src="${element.image}"alt="Shoes" class="rounded-xl"/>
-            </figure>
-            <div class="card-body items-center">
-                <h2 class="text-2xl font-semibold mt-6">Features</h2>
+            <div class="card bg-base-100 shadow-xl border p-6 rounded-xl">
+                <figure>
+                    <img src="${element.image}"alt="Shoes" class="rounded-xl"/>
+                </figure>
+                <div class="card-body items-center">
+                    <div class="features my-6">
+                        <h2 class="text-2xl font-semibold  mb-4">Features</h2>
+                    </div>
+
+                    <div class="py-6 border-t-2 flex items-center justify-between">
+                        <div>
+                            <h2 class="text-3xl font-semibold mb-4 ">${element.name}</h2>
+                            <p><i class="fa-regular fa-calendar-days mr-2 text-[#585858]"></i>${element.published_in}</p>
+                        </div>
+                        <button>
+                            <i class="fa-solid fa-arrow-right text-[#EB5757] bg-[#FEF7F7] p-4 rounded-full"></i>
+                        </button>
+                    </div>
+                </div>
             </div>
-        </div>
-        `
+        `;
+        // append the ol to the features
+        const features = card.querySelector(".features");
+        features.appendChild(ol); 
         cardsContainer.appendChild(card);
     });
 }
 
-
-
 loadAllData()
+
+  
